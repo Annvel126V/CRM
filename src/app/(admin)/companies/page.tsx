bca1bdf93f2d2502ba528c1de155524a98d155dd
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2025-06-04 16:04:34 +0200

    Save before codemod

---
+++ src/app/(admin)/companies/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from '../../components/header';
+import Toolbar from '../../components/toolbar';
+import SearchInput from '../../components/search-input';
+import AddCompanyButon from '../../components/add-company-button';
+import CompanyTable from '../../components/company-table';
+import CompanyRow from '../../components/company-row';
+import { Status } from '../../components/status-label';
+
+export default function Page() {
+  return (
+    <>
+      <Header>Companies</Header>
+      <main>
+        <Toolbar action={<AddCompanyButon />}>
+          <SearchInput />
+        </Toolbar>
+        <CompanyTable>
+          <CompanyRow
+            id={1}
+            category="Products"
+            company="Costco"
+            status={Status.Pending}
+            promotion={true}
+            country="USA"
+            joineDate="02.19.2023"
+          />
+        </CompanyTable>
+      </main>
+    </>
+  );
+}
---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MagicButton from '@/app/components/magic-button';
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-xl">Dashboard Page</h1>
+      <MagicButton />
+    </main>
+  );
+}
