9a752ff52c1745f20683fa09906bac037add39bd
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2025-06-06 17:21:53 +0200

    add dashboard

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import MagicButton from '@/app/components/magic-button';
 
 export default function Page() {
   return (
     <main>
       <h1 className="text-xl">Dashboard Page</h1>
-      <MagicButton />
     </main>
   );
 }
