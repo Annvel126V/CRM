0571352c7ee6b4eb594b56ddcede8d0d183e612c
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2025-07-18 21:06:58 +0200

    fix import in modal-new-page

--- src/app/(admin)/companies/@modal/(.)new/page.tsx
+++ src/app/(admin)/companies/@modal/(.)new/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import CompanyFormModal from '@/app/components/company-form-modal';
 
 export default function Page() {
