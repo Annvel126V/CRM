c8a4580f5311d50d0ac6756b892d67b9b0f78ada
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2025-07-21 00:29:05 +0200

    add log for chek

--- src/app/components/company-form-modal.tsx
+++ src/app/components/company-form-modal.tsx
@@ -1,21 +1,13 @@
 'use client';
 
 import React from 'react';
-import CompanyForm, { CompanyFieldValues } from './company-form';
+import CompanyForm from './company-form';
 import Modal, { ModalProps } from './modal';
 
-interface CompanyFormModalProps extends ModalProps {
-  onSubmit?: (values: CompanyFieldValues) => void | Promise<void>;
-}
-
-export default function CompanyFormModal({
-  onClose,
-  onSubmit,
-  ...rest
-}: CompanyFormModalProps) {
+export default function CompanyFormModal({ onClose, ...rest }: ModalProps) {
   return (
     <Modal {...rest} onClose={onClose}>
-      <CompanyForm onSubmit={onSubmit ?? (() => onClose())} />
+      <CompanyForm onSubmit={() => onClose()} />
     </Modal>
   );
 }
